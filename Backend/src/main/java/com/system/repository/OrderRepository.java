441036605f7555a27daee534ae4e9cf179c1592a
Thuvarakan <thuvarakanp96@gmail.com>
2023-05-06 18:16:11 +0530

    app commit

---
+++ Backend/src/main/java/com/system/repository/OrderRepository.java
@@ -0,0 +1,7 @@
+package com.system.repository;
+
+import com.system.model.Orders;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface OrderRepository extends JpaRepository<Orders,Integer> {
+}
